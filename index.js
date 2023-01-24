aaf3cf4135b5641cd72d6ec29ea2dc22684a6f1d
muthu ram <mr032495@gmail.com>
2023-01-24 21:44:16 +0530

    insertion

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ const dbo = require("./db");
 app.engine('hbs',exhbs.engine({layoutsDir:'views/',defaultLayout:"main",extname:'hbs'}))
 app.set('view engine','hbs');
 app.set('views','views')
-
+app.use(bodyparser.urlencoded({ extended: true }))
 app.get('/',async(req,res)=>{
     let database =await dbo.getDataBase();
     const collection = database.collection('books');
     const cursor = collection.find({})
     let employees =await cursor.toArray();
     let message = '';
+    
+    switch(req.query.status ){
+        case '1':
+            message= "Inserted sucessfully"
+            break;
+    }
     res.render('main',{message,employees})
+})
 
+
+app.post('/store_book',async(req,res)=>{
+    let database =await dbo.getDataBase();
+    const collection = database.collection('books');
+    let book = { title: req.body.title, author: req.body.author }
+    await collection.insertOne(book);
+    return res.redirect('/?status=1')
 })
 app.listen(8000,()=>{console.log('8000')})
